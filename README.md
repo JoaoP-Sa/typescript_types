bc4ee725fcf8047a8b00e61f14fc523c55ea4940
João <joao.pdesa@hotmail.com>
2022-01-08 01:37:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Códigos em ts que criei com base em um curso de typescript que estou fazendo pelo youtube, do canal academind, a função criada no typescript utiliza todos os tipos de valores primitivos: string, number e boolean.
+
+Ts codes that i created in a typescript course that i'm mading by youtube, of academind channel, the created function in typescript use all od primitive values types: string, number and boolean.
---
+++ functions.js
@@ -0,0 +1,35 @@
+function add(n1, n2, showResult, phrase) {
+    // if(typeof number1 !== 'number' || typeof number2 !== 'number'){
+    //   throw new Error('Incorrect input!')
+    // } código para trazer uma mensagem de erro personalizada caso algum dos parâmetros não seja do tipo number
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'The result is: ';
+add(number1, number2, printResult, resultPhrase);
+/*no javascript temos vários tipos de dados, que são:
+
+    - number: o javascript não diferencia tipos de número, nele não há diferença entre int, float, double,
+      etc
+
+    - string: são os valores que são interpretados como texto, no javascript podemos declarar uma string
+      usando "", '', e ``
+
+    - boolean: valores booleanos são os de true e false
+
+  Ao declararmos uma variável ou um parâmetro no typescript nós podemos especificar qual será o tipo daquele
+  valor, no caso de uma função podemos especificar seus valores assim da seguinte forma:
+    function(num1: number, num2: number){
+      return num1 + num2;
+    }
+                                        * O tipo number nos parâmetros foi só um exemplo, também poderíamos ter
+                                          colocado string e boolean se quiséssemos
+*/
